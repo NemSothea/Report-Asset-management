edce74395e8ba13bcaf02123f8fe091d09c0fea1
sothea007 <nemsothea13@gmail.com>
2024-09-16 08:07:08 +0700

    Initial Commit

---
+++ Report Asset management/Report_Asset_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Report_Asset_managementApp.swift
+//  Report Asset management
+//
+//  Created by sothea007 on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Report_Asset_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
